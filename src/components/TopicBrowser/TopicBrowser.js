54bf91e08b21a1f432e7705ebd1a720aa0e12e1f
Jessica Swenson <jessicamswens@gmail.com>
2022-08-14 19:30:52 -0600

    first component TopicBrowser

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Component from 'react';
+
+
+const TopicBrowser = () => {
+
+    return (
+        <div>
+            <p>Hello World</p>
+        </div>
+    )
+}
+
+
+export default TopicBrowser;
